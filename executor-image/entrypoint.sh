3463fe13b8f444da8224bf2dc1259adf8f27bbd7
Vincenzo Eduardo Padulano <v.e.padulano@gmail.com>
2019-09-06 17:40:07 +0200

    latest attempts with docker

--- executor-image/entrypoint.sh
+++ executor-image/entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsrel CMSSW_11_0_ROOT618_X_2019-08-28-2300
-cd CMSSW_11_0_ROOT618_X_2019-08-28-2300
-cmsenv
+source /cmssw_setup.sh
 /bin/bash ${SPARK_HOME}/kubernetes/dockerfiles/spark/entrypoint.sh "$@"
