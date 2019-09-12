c8129218d2397379cdb0e7fabce8d650dbf13afb
vepadulano <v.e.padulano@gmail.com>
2019-09-12 15:55:57 +0200

    entrypoint sources cmssw

--- executor-image/cmssw_setup.sh
+++ executor-image/cmssw_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
-scramv1 project CMSSW_11_0_ROOT618_X_2019-08-28-2300
-cd CMSSW_11_0_ROOT618_X_2019-08-28-2300
+scramv1 project CMSSW_11_0_ROOT618_X_2019-09-05-2300
+cd CMSSW_11_0_ROOT618_X_2019-09-05-2300
 eval `scramv1 runtime -sh`
